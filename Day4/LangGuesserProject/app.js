498e7d06f59fa395bab6ed8ad3b84c7723b2b099
Venkat Sunkara <venkatsunkara9959@gmail.com>
2026-07-22 14:00:34 +0530

    done

--- Day4/LangGuesserProject/app.js
+++ Day4/LangGuesserProject/app.js
@@ -2,32 +2,32 @@ import { franc } from "franc";
 import langs from 'langs'
 import 'colors'
 
-const input = process.argv[2]
+const input = process.argv[2];
+
 const language = franc(input)
+
 try {
     if (!input) {
-        console.log("Please Provide Text input..!".red);
+        console.log("Please provide text...!".red);
+
     }
     else {
         const languageCode = langs.where('3', language)
         if (languageCode === undefined) {
-            console.log("Undefined Language ...Try Again".red);
+            console.log("Language not found...".red);
         }
         else {
             if (languageCode && languageCode.name) {
                 console.log(languageCode.name.green);
             }
             else {
-                console.log(`Found Code : ${languageCode} but no name found`.red);
+                console.log("Language name not Found...!".red);
+
             }
         }
     }
-
-} catch (error) {
-    console.log("Error..Try with other sample text !!!".red);
+}
+catch (e) {
+    console.log("Try with another sample text..!".red);
 
 }
-
-
-
-
