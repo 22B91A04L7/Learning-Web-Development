f271c07588fceaa3d618f2c4ebf60705996e8f20
Venkat Sunkara <venkatsunkara9959@gmail.com>
2026-07-25 14:49:04 +0530

    # new file:   .gitignore 	modified:   Day3/JokeGenerator/.gitignore 	modified:   Day4/LangGuesserProject/.gitignore 	modified:   JavaScript/forEachMethod/server.js

--- JavaScript/forEachMethod/server.js
+++ JavaScript/forEachMethod/server.js
@@ -1,34 +1,34 @@
 const arr = [1, 2, 3, 4, 5];
 
-function print(ele){
+function print(ele) {
     console.log(ele);
 }
 
 arr.forEach(print);
 
-arr.forEach(function(ele){
-    console.log(`Sqaure of ${ele} is ${ele*ele}`);
+arr.forEach(function (ele) {
+    console.log(`Sqaure of ${ele} is ${ele * ele}`);
 })
 //forEach is used to access each elements in an array
-arr.forEach(function(e){
-    try{
+arr.forEach(function (e) {
+    try {
         console.log("NUmber :" + e);
     }
-    catch{
+    catch {
         console.log("Some Error !!");
     }
 })
 //newer method is for of method where the usecase is same to forEach
-for(let i of arr){
+for (let i of arr) {
     console.log("Value of i : " + i);
 }
 
-arr.forEach(function (ele){
-    
-        if(ele % 2 == 0){
-            console.log(ele);
-        }
-        else 
+arr.forEach(function (ele) {
+
+    if (ele % 2 == 0) {
+        console.log(ele);
+    }
+    else
         console.log("Number is not Even");
-  
+
 })
