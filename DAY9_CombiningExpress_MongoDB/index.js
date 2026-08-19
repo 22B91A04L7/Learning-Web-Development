2c8f5bdeb74e1f8e0d88725daffaad4442a1ff8e
Venkat Sunkara <venkatsunkara9959@gmail.com>
2026-08-19 14:36:38 +0530

    CRUD ADDED!

--- DAY9_CombiningExpress_MongoDB/index.js
+++ DAY9_CombiningExpress_MongoDB/index.js
@@ -3,9 +3,11 @@ const app = express();
 const path = require("path")
 const mongoose = require("mongoose")
 const Product = require("./models/product")
+const methodOverride = require("method-override")
 
 
 const MONGO_URI = "mongodb://localhost:27017/Demo"
+const categories = ['fruit', 'vegetable', 'dairy']
 
 mongoose.connect(MONGO_URI)
     .then(() => {
@@ -20,10 +22,32 @@ app.set('views', path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(methodOverride('_method'))
+
+app.delete("/products/:id", async (req, res) => {
+    const { id } = req.params;
+    await Product.findByIdAndDelete(id)
+    res.redirect("/products")
+})
+
+//form to edit individual product
+app.get("/products/:id/edit", async (req, res) => {
+    const { id } = req.params
+    const product = await Product.findById(id)
+    res.render("products/edit", { product, categories })
+})
+
+//post the edit in server
+app.put("/products/:id", async (req, res) => {
+    const { id } = req.params
+    const product = req.body
+    await Product.findByIdAndUpdate(id, product, { runValidators: true })
+    res.redirect(`/products/${id}`)
+})
 
 //form to add product
 app.get("/products/new", (req, res) => {
-    res.render("products/new")
+    res.render("products/new", { categories })
 })
 
 // adding product
