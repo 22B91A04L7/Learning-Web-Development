c430cda89b845e56a68b3533f91f199f4326bbf6
Venkat Sunkara <venkatsunkara9959@gmail.com>
2026-08-22 12:51:12 +0530

    intro to custom middlewares

--- DAY10_IntroToMiddlewares/index.js
+++ DAY10_IntroToMiddlewares/index.js
@@ -3,6 +3,17 @@ const app = express();
 const morgan = require("morgan")
 
 app.use(morgan('tiny'));
+app.use((req, res, next) => {
+    console.log("THIS IS FIRST MIDDLEWARE!");
+    return next();
+    console.log("FINAL");
+
+})
+app.use((req, res, next) => {
+    console.log("THIS IS SECOND MIDDLEWARE!");
+    next();
+    console.log("FINAL 222");
+})
 
 app.get("/", (req, res) => {
     res.send("HOME PAGE !!")
